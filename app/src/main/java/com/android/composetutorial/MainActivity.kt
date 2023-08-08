8b84c04c670cbe7bcad82a4f562e33c1939a2443
clauRTmfg <crta4.0@gmail.com>
2023-08-08 15:30:28 -0300

    Adicionando expansão das msgs com animação e mudança de cor

--- app/src/main/java/com/android/composetutorial/MainActivity.kt
+++ app/src/main/java/com/android/composetutorial/MainActivity.kt
@@ -18,28 +18,48 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.android.composetutorial.ui.theme.ComposeTutorialTheme
 import android.content.res.Configuration
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        var x = 0
         setContent {
             ComposeTutorialTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    MessageCard(Mensagem("clauRTmfg", "Desenvolvendo com Jetpack Compose"))
-                }
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colorScheme.background
+//                ) {
+//                    MessageCard(Message("clauRTmfg", "Desenvolvendo com Jetpack Compose"))
+//                }
+                Conversation(SampleData.conversationSample)
             }
         }
     }
 }
 
-data class Mensagem(val nome: String, val descricao: String)
 
 @Composable
-fun MessageCard(msg: Mensagem) {
+fun ClickCounter(clicks: Int, onClick: () -> Unit) {
+    Button(onClick = onClick) {
+        Text("I've been clicked $clicks times")
+    }
+}
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(msg: Message) {
 
     Row(modifier = Modifier.padding(all = 8.dp)) {
         Image(
@@ -50,26 +70,62 @@ fun MessageCard(msg: Mensagem) {
                 .clip(CircleShape)
                 .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
         )
-
         Spacer(modifier = Modifier.width(8.dp))
 
-        Column {
+        // We keep track if the message is expanded or not in this
+        // variable
+        var isExpanded by remember { mutableStateOf(false) }
+
+        // surfaceColor will be updated gradually from one color to the other
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
+            label = "",
+        )
+
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
             Text(
-                text = msg.nome,
+                text = msg.author,
                 color = MaterialTheme.colorScheme.secondary,
                 style = MaterialTheme.typography.titleSmall
             )
 
             Spacer(modifier = Modifier.height(4.dp))
 
-            Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp) {
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                shadowElevation = 1.dp,
+                // surfaceColor color will be changing gradually from primary to surface
+                color = surfaceColor,
+                // animateContentSize will change the Surface size gradually
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
                 Text(
-                    text = msg.descricao,
+                    text = msg.body,
                     modifier = Modifier.padding(all = 4.dp),
+                    // If the message is expanded, we display all its content
+                    // otherwise we only display the first line
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
                     style = MaterialTheme.typography.bodyMedium
                 )
             }
         }
+    }
+}
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversation() {
+    ComposeTutorialTheme {
+        Conversation(SampleData.conversationSample)
     }
 }
 
@@ -83,7 +139,7 @@ fun MessageCard(msg: Mensagem) {
 fun DefaultPreview() {
     ComposeTutorialTheme {
         Surface {
-            MessageCard(Mensagem("ClauRT", "Testando Jetpack Compose !!"))
+            MessageCard(Message("ClauRT", "Testando Jetpack Compose !!"))
         }
     }
 }
---
+++ app/src/main/java/com/android/composetutorial/SampleData.kt
@@ -0,0 +1,72 @@
+package com.android.composetutorial
+
+object SampleData {
+    // Sample conversation data
+    val conversationSample = listOf(
+        Message(
+            "clauRTmfg",
+            "Test...Test...Test..."
+        ),
+        Message(
+            "clauRTmfg",
+            """List of Android versions:
+            |Android KitKat (API 19)
+            |Android Lollipop (API 21)
+            |Android Marshmallow (API 23)
+            |Android Nougat (API 24)
+            |Android Oreo (API 26)
+            |Android Pie (API 28)
+            |Android 10 (API 29)
+            |Android 11 (API 30)
+            |Android 12 (API 31)""".trim()
+        ),
+        Message(
+            "clauRTmfg",
+            """I think Kotlin is my favorite programming language.
+            |It's so much fun!""".trim()
+        ),
+        Message(
+            "clauRTmfg",
+            "Searching for alternatives to XML layouts..."
+        ),
+        Message(
+            "clauRTmfg",
+            """Hey, take a look at Jetpack Compose, it's great!
+            |It's the Android's modern toolkit for building native UI.
+            |It simplifies and accelerates UI development on Android.
+            |Less code, powerful tools, and intuitive Kotlin APIs :)""".trim()
+        ),
+        Message(
+            "clauRTmfg",
+            "It's available from API 21+ :)"
+        ),
+        Message(
+            "clauRTmfg",
+            "Writing Kotlin for UI seems so natural, Compose where have you been all my life?"
+        ),
+        Message(
+            "clauRTmfg",
+            "Android Studio next version's name is Arctic Fox"
+        ),
+        Message(
+            "clauRTmfg",
+            "Android Studio Arctic Fox tooling for Compose is top notch ^_^"
+        ),
+        Message(
+            "clauRTmfg",
+            "I didn't know you can now run the emulator directly from Android Studio"
+        ),
+        Message(
+            "clauRTmfg",
+            "Compose Previews are great to check quickly how a composable layout looks like"
+        ),
+        Message(
+            "clauRTmfg",
+            "Previews are also interactive after enabling the experimental setting"
+        ),
+        Message(
+            "clauRTmfg",
+            "Have you tried writing build.gradle with KTS?"
+        ),
+    )
+}
